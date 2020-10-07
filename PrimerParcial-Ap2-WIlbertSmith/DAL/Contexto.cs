0eaf5e1bb67c3cc48a975d857be9fa5168d26a7b
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-10-07 07:21:30 -0500

    Add project files.

---
+++ PrimerParcial-Ap2-WIlbertSmith/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimerParcial_Ap2_WIlbertSmith.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrimerParcial_Ap2_WIlbertSmith.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Productos> productos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\ProductoDB.db");
+        }
+    }
+}
